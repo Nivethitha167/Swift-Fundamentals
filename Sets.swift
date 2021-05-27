f949bcd32172bcec0f2b6ebfbc97e2002d4b48ed
Nivethitha SoundarRajan <52025677+Nivethitha167@users.noreply.github.com>
2021-05-27 12:18:44 +0530

    Create Sets.swift

---
+++ Sets.swift
@@ -0,0 +1,53 @@
+//---------------sets-----------
+// for storing unique values in unordered way
+var arr = Set<Int>()
+arr = [3, 7, 8]
+print(arr)
+
+arr = []
+print(arr)
+
+var arr1: Set<Int> = [1, 2, 3]
+print(arr1)
+
+var arr2: Set<Character> = ["a", "b"]
+print(type(of: arr2))
+
+var arr3: Set = ["abc", "def"]
+print(type(of: arr3))
+
+var arr4: Set<Int>?
+print(type(of: arr4))
+
+//-------------------------
+
+arr1.count
+arr1.insert(7)
+print(arr1)
+
+arr1.insert(1)
+print(arr1)
+
+arr1.insert(6)
+print(arr1)
+
+arr1.remove(7)
+print(arr1)
+
+arr1.sorted() // it does not change original set value - so it needs another set to hold the values
+print(arr1)
+
+var arr5 = arr1.sorted()
+print(arr5)
+
+//there is no sort() for sets - only sorted()
+// for array we have both sort() and sorted()
+//sort() - sort the array itself
+//sorted() - needs another array to place sorted values or temporarily sorts the value
+
+//--------------------------traversing---------
+for (i,v) in arr1.enumerated()
+{
+    print(i,terminator: " ")
+    print(v)
+}
